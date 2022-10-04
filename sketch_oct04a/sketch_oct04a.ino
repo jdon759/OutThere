5a6c3f0ea9c533591e6cd226949cc2bebe194d1b
James Donaldson <jdon759@UoA.auckland.ac.nz>
2022-10-04 20:55:10 +1300

    Added start of music

---
+++ sketch_oct04a/sketch_oct04a.ino
@@ -0,0 +1,23 @@
+#include <MusicWithoutDelay.h>
+#include <synth.h>
+#include <tables.h>
+
+#define SOURCE https://musescore.com/user/27824718/scores/4941073
+
+/*rest is p*/
+// + means slur
+// , is slight pause
+// 
+
+const char blue_danube[] PROGMEM = " Blue Danube Waltz : b=90 : c+e+g, g ";
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
